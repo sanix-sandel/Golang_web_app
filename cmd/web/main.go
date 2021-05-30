eadfdae03491081b63e4f4ee6529ceff45cf3173
sanix <sanicksikani@gmail.com>
2021-05-30 19:12:47 +0300

    Logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	//Create a logger for writing information messages. This three parameters:
+	//the destination to write the logs to (os.Stdout), a st prefix for message(INFO followed by a tab)
+	//and flags to indicate what additional information to include (local data and time). Note that are
+	//joined using the bitwise OR operator |.
+	//
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/create", createSnippet)
@@ -14,7 +28,16 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	//Initialize a new http.Server strcut
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+
+	//logger for error message
 }
